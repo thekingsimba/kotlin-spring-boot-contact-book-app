aada520a7c5adabc31f08ecc9633cf2e06364c9d
Salomon AYAH <salomonayah@gmail.com>
2024-01-08 04:11:13 +0100

    project init

---
+++ src/main/kotlin/com/phonebookbackend/KlotlinTaskApplication.kt
@@ -0,0 +1,11 @@
+package com.phonebookbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KlotlinTaskApplication
+
+fun main(args: Array<String>) {
+	runApplication<KlotlinTaskApplication>(*args)
+}
---
+++ src/test/kotlin/com/phonebookbackend/KlotlinTaskApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.phonebookbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KlotlinTaskApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
